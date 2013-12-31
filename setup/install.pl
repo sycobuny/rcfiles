58afd10b5fd3496320ef50028201331664d65f1d
Stephen Belcher <sbelcher@gmail.com>
2013-12-31 16:06:56 -0500

    Add basic `rcfiles` manager and initial installer
    The `rcfiles` binary is for managing this repository from a place
    external to it, as it is located along the $PATH specified by both
    ~/.bashrc and ~/.zshrc. It is executed via `rcfiles <subcommand>`. The
    following subcommands are currently available:
    
     * relink - Move existing dot-rc files from $HOME (or wherever they are
       linked to) into the current directory, and relink them there. This
       command can be used to import existing differences from current rc
       files, or discarding them, using standard git commands.
     * autoconfig - Set up the repository-local git configuration with
       necessary settings for proper functionality. Currently this means
       applying a filter so that certain semi-private information (name and
       email address) are not committed to the repository by accident.
     * status - Lets you know whether there are any differences between the
       working copy and the repository. It is a shorthand for both running
       `git status` in the rcfiles repository as well as its output.
     * update - Attempts to pull changes from the remote rcfiles repository.
    
    `rcfiles` is also written in such a way that it can be included as a
    Perl module, though it does not return a true value and this must be
    accounted for. All these subcommands are methods in the RCFiles package.
    
    The installer script is written in a way that it can both be locally
    executed, as well as piped into `perl` and executed from a download (ie,
    that way tons of people use but almost everyone else hates). Everybody
    wins! Soon I'll write a README which details its usage.

---
+++ setup/install.pl
@@ -0,0 +1,153 @@
+#!/usr/bin/env perl
+
+use warnings;
+use strict;
+
+use File::Basename qw(dirname);
+use File::Spec;
+
+use constant DEFAULT_RCDIR  => File::Spec->catfile($ENV{HOME}, '.rc');
+use constant DEFAULT_REMOTE => 'git@github.com:sycobuny/rcfiles.git';
+
+sub verify_tools {
+    my (@required) = qw(perl bash);
+    my (@optional) = qw(git zsh);
+    my ($failed)   = 0;
+    my ($missing)  = 0;
+
+    print "=> Checking for missing tools...\n";
+
+    foreach my $required (@required) {
+        $ENV{TOOL} = $required;
+        `which "\$TOOL" 2>&1`;
+
+        if ($?) {
+            warn "Could not find required tool `$required` in your \$PATH\n";
+            $failed++;
+        }
+    }
+
+    die "fatal: missing $failed required tool(s)\n" if $failed;
+
+    foreach my $optional (@optional) {
+        $ENV{TOOL} = $optional;
+        `which "\$TOOL"`;
+
+        if ($?) {
+            warn "Could not find recommended tool `$optional` in your " .
+                 "\$PATH\n";
+            $failed++;
+        }
+    }
+
+    if ($failed) {
+        warn "Missing $failed optional tools, attempting to continue...\n";
+    }
+    else {
+        print "=> All tools found\n";
+    }
+}
+
+sub resolve_dir {
+    my ($breakout) = sub {
+        $ENV{RCDIR} = shift;
+        print "=> Resolved \$RCDIR to $ENV{RCDIR}\n";
+
+        if (shift && $ENV{RCDIR} ne DEFAULT_RCDIR) {
+            $ENV{FILE} = File::Spec->catfile($ENV{HOME}, '.rcdir');
+
+            print "=> Saving \$RCDIR to $ENV{FILE}\n";
+            `echo "\$RCDIR" > "\$FILE"`;
+        }
+
+        $ENV{RCDIR};
+    };
+
+    return $breakout->($ENV{RCDIR}, 1) if $ENV{RCDIR};
+    return $breakout->(DEFAULT_RCDIR)  if (__FILE__ eq '-');
+
+    # we'll assume we're being run from inside the proper git repo, then.
+    chdir(dirname(__FILE__));
+    my ($rcdir) = `git rev-parse --show-toplevel 2>/dev/null`;
+    chomp($rcdir);
+
+    # but maybe we don't have git, or otherwise can't resolve it? use the
+    # default if we failed to get the root of the repo for whatever reason.
+    return $breakout->(DEFAULT_RCDIR) unless $rcdir;
+
+    # resolve the given rcdir to an absolute path and use that
+    $breakout->(File::Spec->rel2abs($rcdir), 1);
+}
+
+sub clone_repository {
+    $ENV{RC_GIT_REMOTE} ||= DEFAULT_REMOTE;
+
+    print "=> Attempting to clone repository...\n";
+    my ($output) = `git clone "\$RC_GIT_REMOTE" "\$RCDIR" 2>&1`;
+
+    if ($? && $output =~ /already exists and is not an empty directory/) {
+        warn "Directory $ENV{RCDIR} already exists, attempting to " .
+             "continue...\n";
+
+        switch_to_repository();
+
+        `git init .`;
+        die "Could not init $ENV{RCDIR} as a git repository: $!\n" if $?;
+
+        my ($url) = `git config remote.origin.url`;
+        chomp($url) if $url;
+
+        if (!$url) {
+            `git remote add origin "\$RC_GIT_REMOTE" 2>/dev/null`;
+            die "fatal: Could not add remote $ENV{RC_GIT_REMOTE}\n" if $?;
+
+            print "=> Set remote 'origin' to $ENV{RC_GIT_REMOTE}\n";
+        }
+        elsif ($url ne $ENV{RC_GIT_REMOTE}) {
+            die "fatal: Remote 'origin' already set to $url\n";
+        }
+
+        `git fetch origin master 2>/dev/null`;
+        die "fatal: Could not synchronize from 'origin'\n" if $?;
+        print "=> Pulled all remote changes\n";
+
+        `git merge --ff-only origin/master`;
+        if ($?) {
+            die "fatal: Could not merge upstream changes from " .
+                "'origin/master'\n";
+        }
+        print "=> Updated repository to latest changes\n";
+    }
+}
+
+sub switch_to_repository {
+    chdir($ENV{RCDIR})
+        or die "fatal: Failed to change directory to $ENV{RCDIR}: $!\n";
+}
+
+sub load_library {
+    do './bin/rcfiles';
+    die "fatal: Failed to load bin/rcfiles library: $@\n" if $@;
+    die "fatal: Failed to load bin/rcfiles library: $!\n" if $!;
+}
+
+
+# check for required (mostly) tools
+verify_tools();
+
+# figure out where we need to put the repository
+resolve_dir();
+
+# bootstrap the base repository and switch into it for more work
+clone_repository();
+switch_to_repository();
+
+# load the command line library
+load_library();
+
+# re-link all the configuration files to point into our system
+RCFiles::relink();
+
+# set up the local git configuration, currently, this:
+#   * sets a filter on ~/.gitconfig to ignore [user] sections
+RCFiles::autoconfig();
