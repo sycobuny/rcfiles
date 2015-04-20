a7a28ebf271347b65932dbbfb429d15eb829d7fa
Stephen Belcher <sbelcher@gmail.com>
2015-04-20 12:01:19 -0400

    Alter strip-ini-section to use STDIN instead of fn
    Speaking of old git versions, the '%f' expansion in the filter.x.clean
    option is newer than some of the versions of git I use, and in this
    particular case, is actually unneeded - the script already gets fed the
    contents into STDIN, so all we really need to do is just read that. It
    makes the 'strip-ini-section.pl' file a bit less reusable, but then, I'm
    not really using it anywhere else, so whatever I guess.

--- setup/strip-ini-section.pl
+++ setup/strip-ini-section.pl
@@ -3,19 +3,10 @@
 use warnings;
 use strict;
 
-my ($filename) = shift;
 my ($section)  = shift;
 my ($pattern)  = qr/^\s*\[(.*)\]\s*$/;
-my ($handle, $current_section, @lines);
-
-die "fatal: file does not exist: $filename\n"  unless (-e $filename);
-die "fatal: file is not readable: $filename\n" unless (-r $filename);
-die "fatal: not a filename: $filename\n"       unless (-f $filename);
-die "fatal: could not open $filename for reading\n"
-    unless (open($handle, $filename));
-
-@lines = <$handle>;
-close($handle);
+my (@lines)    = <>;
+my ($current_section);
 
 foreach my $line (@lines) {
     if ($line =~ $pattern) { $current_section = $1 }
