5593f2f80d8081f801a9714dc0a6d76796477f61
Stephen Belcher <sbelcher@gmail.com>
2017-01-07 09:32:04 -0500

    Add flag to $LESS to preserve colors
    Without the -r flag, color codes are not passed through to the terminal
    properly, resulting in some really unfortunate displays. So, let's add
    that flag into $LESS.

--- configs/shell/environment.sh
+++ configs/shell/environment.sh
@@ -16,7 +16,7 @@ if [ $(uname -a | grep Darwin | wc -l) -ne 0 ]; then
     source $RCDIR/configs/shell/environment-mac.sh
 fi
 
-export LESS=-MN
+export LESS=-rMN
 
 # perl versioning - plenv is preferred, if not, maybe perlbrew is still around
 # note that perlbrew and plenv do not play together. they have inherited their
